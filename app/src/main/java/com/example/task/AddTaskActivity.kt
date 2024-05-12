b6644b22e2bab631d599ca210f5ec5ce8ca9141b
SheharaTharushi <tshehara7@gmail.com>
2024-05-12 10:41:41 +0530

    push 4

---
+++ app/src/main/java/com/example/task/AddTaskActivity.kt
@@ -0,0 +1,34 @@
+package com.example.task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.example.task.databinding.ActivityAddTaskBinding
+
+
+class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+    private lateinit var db: TaskDatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TaskDatabaseHelper(this)
+
+        binding.savebutton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val task = Task(0, title)
+            db.insertTask(task)
+            finish()
+            Toast.makeText(this, "Task Saved", Toast.LENGTH_SHORT).show()
+
+
+        }
+    }
+
+
+}
